fa8447efdbc2a6fddccb30641f0b258461273b9a
Josh <panomosh@gmail.com>
2017-02-07 14:44:42 +0000

    more updates

--- public/app/services/shipDataCache.service.js
+++ public/app/services/shipDataCache.service.js
@@ -6,7 +6,7 @@ angular.module('app').factory('shipDataCache', function(){
             {
                 name: "Ion-Pickaxe mk1",
                 price: 100,
-                description: "The bog standard reliable imperial mining ship.",
+                description: "The bog standard reliable imperial mining ship. Fires a concentracted ion pulse which breaks away small chunks of ore.",
                 orePerTick: 0.1,
                 amountOwned: 1,
                 imageUrl: 'http://wiki.eveuniversity.org/images/e/ef/Venture.jpg'
@@ -17,7 +17,7 @@ angular.module('app').factory('shipDataCache', function(){
                 description: "The Gravity Hammer is a mid size ship that utilises precise blasts of gravitational energy to dislodge chunks of ore in large amounts.",
                 orePerTick: 1.5,
                 amountOwned: 0,
-                imageUrl: 'http://www.igorstshirts.com/blog/conceptships/2012/kian_ng/kian_ng_04.jpg'
+                imageUrl: 'http://images.mediabiz.de/newspics/198/414198_1/b450x254.jpg'
             },
             {
                 name: "Titan X5",
